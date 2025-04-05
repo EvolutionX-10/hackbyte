720469739b957acb5a3356fad197ebc1eed3f2f9
Ankit Pande <pandear@rknec.edu>
2025-04-05 15:59:01 +0530

    bot works?? maybe

--- backend/dataset.py
+++ backend/dataset.py
@@ -11,7 +11,7 @@ def download_stock_data(ticker, period='1mo', interval='1m'):
     df = yf.download(ticker, period=period, interval=interval)
     df.dropna(inplace=True)
     df = df.reset_index()  # Move datetime index to a column
-    df['Datetime'] = pd.to_datetime(df['Datetime'])  # Ensure it's properly formatted
+    df['atetime'] = pd.to_datetime(df['Datetime'])  # Ensure it's properly formatted
     return df
 
 def compute_technical_indicators(df):
